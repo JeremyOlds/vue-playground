c638931c884953dfec89a2bc6ad84e3a89164a16
Jeremy <ayamaikuko@gmail.com>
2023-07-17 14:48:11 -0600

    completed exercises

--- src/components/ExpressionsExercise.vue
+++ src/components/ExpressionsExercise.vue
@@ -9,16 +9,51 @@
             below:
           </p>
 
-          <p>{{}}</p>
+          <p>25 x 15 = {{total}}</p>
         </div>
         <div class="border p-1">
           <p>
             Use interpolation to get the customary Hello, World! to display on
             the screen. Don't forget your quotes
           </p>
-          <p>{{}}</p>
+          <p>{{helloWorld()}}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+
+<script>
+import { computed } from "vue";
+
+
+export default{
+  setup(){
+
+
+
+
+    return{
+      total: computed(() => {
+        let total = 25 * 15
+        return total
+      }),
+
+      helloWorld(){
+        let quote = '"Hello World!"'
+        return quote
+      }
+
+
+    }
+  }
+}
+
+
+</script>
+
+<style>
+
+
+</style>
